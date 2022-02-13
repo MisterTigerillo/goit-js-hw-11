0909324ea80b319694f21211ed81b65e175c5843
Dmytro Gobosh <irulka103@ukr.net>
2022-02-13 22:47:18 +0200

    + markup, render & fetch

--- src/index.js
+++ src/index.js
@@ -2,26 +2,41 @@ import './sass/main.scss';
 import PicsApiService from './js/pics-service';
 import { cardListMarkup } from './js/renderCardList';
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
 const form = document.querySelector('.search-form');
-const searchButton = document.querySelector('button');
+// const searchButton = document.querySelector('button');
 const loadMoreButton = document.querySelector('.load-more');
 
 const picsApiService = new PicsApiService();
 
+form.elements.searchQuery.focus();
 form.addEventListener('submit', onFormSubmit);
-
-loadMoreButton.addEventListener('click', onButtonClick);
+loadMoreButton.addEventListener('click', onButtonMoreClick);
 
 function onFormSubmit(e) {
   e.preventDefault();
 
-  picsApiService.query = form.elements.searchQuery.value;
-
-  picsApiService.fetchPics().then(cards => cardListMarkup(cards));
-  // .catch(error => console.log(error));
-  //   console.log(cardData);
+  picsApiService.query = form.elements.searchQuery.value.trim();
+  picsApiService.resetPage();
+  makeCardPack();
 }
 
-function onButtonClick() {
-  picsApiService.fetchPics();
+function onButtonMoreClick() {
+  makeCardPack();
 }
+
+function makeCardPack() {
+  picsApiService.fetchPics().then(cards => {
+    renderCards(cards);
+    let lightbox = new SimpleLightbox('.gallery a');
+  });
+}
+
+function renderCards(cards) {
+  gallery.insertAdjacentHTML('beforeend', cardListMarkup(cards));
+}
+
+// class PicsApiService -> picsApiService ->   (m) fetchPics   <--- renderCards -> cardListMarkup
--- src/js/pics-service.js
+++ src/js/pics-service.js
@@ -1,31 +1,47 @@
+import { Notify } from 'notiflix';
+import axios from 'axios';
+
 export default class PicsApiService {
   constructor() {
     this.searchQuery = '';
+    this.page = 1;
   }
 
-  fetchPics() {
-    // console.log(this);
+  async fetchPics() {
     const options = new URLSearchParams({
       key: '4511618-b12f9cdf2c7c50376431dc814',
       q: this.searchQuery,
       image_type: 'photo',
       orientation: 'horizontal',
       safesearch: true,
+      per_page: 40,
+      page: this.page,
     });
     const BASE_URL = 'https://pixabay.com/api/';
 
-    return fetch(`${BASE_URL}?${options}`)
-      .then(response => response.json())
-      .then(data => {
-        // console.log(data.hits);
-        return data.hits;
-      });
+    if (this.searchQuery !== '') {
+      console.log(this);
+      return fetch(`${BASE_URL}?${options}`)
+        .then(response => response.json())
+        .then(data => {
+          if (data.hits.length === 0) {
+            Notify.warning(
+              'Sorry, there are no images matching your search query. Please try again.',
+            );
+          }
+          this.page += 1;
+          return data.hits;
+        });
+    }
+  }
+
+  resetPage() {
+    this.page = 1;
   }
 
   get query() {
     return this.searchQuery;
   }
-
   set query(newQuery) {
     this.searchQuery = newQuery;
   }
